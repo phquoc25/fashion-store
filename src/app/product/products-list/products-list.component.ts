61c100704b7a2865a929f205b2faa3d126fd50ea
Quoc <phquoc25@gmail.com>
2021-09-03 21:42:24 +0700

    [products] create input for the product card

--- src/app/product/products-list/products-list.component.ts
+++ src/app/product/products-list/products-list.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Product } from './../shared/product.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-products-list',
     templateUrl: './products-list.component.html',
     styleUrls: ['./products-list.component.scss'],
 })
-export class ProductsListComponent {
-    nbProducts = [1, 2, 3, 4, 5, 6];
+export class ProductsListComponent implements OnInit {
+    products: Product[];
     constructor() {}
+    ngOnInit(): void {
+        this.products = [
+            {
+                id: 1,
+                title: "Premium Denim Women's Hidden",
+                description: 'product description',
+                price: 179,
+                promotion: 15,
+                image: 'pi4.jpg',
+            },
+            {
+                id: 2,
+                title: "Premium Denim Women's Hidden",
+                description: 'product description',
+                price: 179,
+                promotion: 15,
+                image: 'pi.jpg',
+            },
+            {
+                id: 3,
+                title: "Premium Denim Women's Hidden",
+                description: 'product description',
+                price: 179,
+                promotion: 15,
+                image: 'pi5.jpg',
+            },
+            {
+                id: 4,
+                title: "Premium Denim Women's Hidden",
+                description: 'product description',
+                price: 179,
+                promotion: 15,
+                image: 'pi6.jpg',
+            },
+            {
+                id: 5,
+                title: "Premium Denim Women's Hidden",
+                description: 'product description',
+                price: 179,
+                promotion: 15,
+                image: 'pi4.jpg',
+            },
+            {
+                id: 6,
+                title: "Premium Denim Women's Hidden",
+                description: 'product description',
+                price: 179,
+                promotion: 15,
+                image: 'pi4.jpg',
+            },
+        ];
+    }
 }
